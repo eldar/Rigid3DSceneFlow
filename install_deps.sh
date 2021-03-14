787c48ca09d1d97e287c9ad25e2c2b20c2165f50
Eldar Insafutdinov <e.insafutdinov@gmail.com>
2021-03-14 16:50:47 +0000

    dependencies

--- install_deps.sh
+++ install_deps.sh
@@ -1,4 +1,4 @@
-conda install -y -c anaconda coloredlogs pyyaml
-conda install -y -c conda-forge tqdm tensorboardx
-conda install -y -c open3d-admin open3d=0.9.0.0
+conda install -y -c anaconda coloredlogs pyyaml pandas 
+conda install -y -c conda-forge tqdm tensorboardx addict plyfile
+conda install -y -c open3d-admin open3d
 conda install -y -c intel scikit-learn
