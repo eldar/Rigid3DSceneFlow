82e74e7f14b399530e87606826df44c855d9d7e4
Eldar Insafutdinov <e.insafutdinov@gmail.com>
2021-03-15 13:01:09 +0000

    install_deps.sh

--- install_deps.sh
+++ install_deps.sh
@@ -1,4 +1,5 @@
 conda install -y -c anaconda coloredlogs pyyaml pandas 
 conda install -y -c conda-forge tqdm tensorboardx addict plyfile
+conda install -y --file requirements.txt
 conda install -y -c open3d-admin open3d
 conda install -y -c intel scikit-learn
